49cd20b33c3c8dc22d6649164ea683e50c76f485
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-06-26 11:49:17 -0300

    Fixed compiler errors!

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -4,7 +4,7 @@ console.log('💾 >> Initializing...');
 
 const MONGOOSE_CONFIG = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 mongoose.connect(process.env.DB_URI!, MONGOOSE_CONFIG, () => {
   console.log('💾 >> Ready');
 });
+
