c20d0658281e7409c5258fe074148a7f84a8d104
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-07-05 00:57:46 -0300

    Solved cors issues

--- index.ts
+++ index.ts
@@ -1,33 +1,32 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import session from 'express-session';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import session from "express-session";
 
-import config from '@configs/server';
-import handleError from '@helpers/handleError';
-import '@configs/database';
+import config from "@configs/server";
+import handleError from "@helpers/handleError";
+import "@configs/database";
 
-import Users from '@routes/Users';
-import Products from '@routes/Products';
-import Sells from '@routes/Sells';
-import { SESSION_CONFIG } from '@constants/main';
+import Users from "@routes/Users";
+import Products from "@routes/Products";
+import Sells from "@routes/Sells";
+import { SESSION_CONFIG } from "@constants/main";
 
 console.clear();
 
 const app = express();
 const PORT = config.server.port;
+const FRONTEND_APP = process.env.FRONTEND_APP;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: FRONTEND_APP }));
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(handleError);
-app.use(
-  session(SESSION_CONFIG),
-);
+app.use(session(SESSION_CONFIG));
 
-app.use('/api/v1/users', Users);
-app.use('/api/v1/products', Products);
-app.use('/api/v1/sells', Sells);
+app.use("/api/v1/users", Users);
+app.use("/api/v1/products", Products);
+app.use("/api/v1/sells", Sells);
 
 app.listen(PORT, () => {
   console.log(`🦋 >> ${PORT}`);
