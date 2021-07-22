2e2679cb0b586d208221d4cea1248469fe91e812
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-07-22 13:20:54 -0300

    Removed authentication from getProducts and getSingleProduct

--- src/routes/Products/index.ts
+++ src/routes/Products/index.ts
@@ -4,8 +4,8 @@ import isAuth from 'middlewares/isAuth';
 
 const router = express.Router();
 
-router.get('/get', isAuth as never, Products.getProducts);
-router.get('/getSingle/:id', isAuth as never, Products.getSingleProduct);
+router.get('/get', Products.getProducts);
+router.get('/getSingle/:id', Products.getSingleProduct);
 router.post('/create', isAuth as never, Products.createProduct);
 router.put('/edit/:id', isAuth as never, Products.editProduct);
 router.delete('/delete/:id', isAuth as never, Products.deleteProduct);
