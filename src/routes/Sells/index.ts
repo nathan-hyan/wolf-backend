24e8443fd6076d6d294aefb2a65c45ba809d8fd0
Hy-An <nathan.hyan2048@gmail.com>
2021-09-06 15:55:58 -0300

    Toggle finished on product

--- src/routes/Sells/index.ts
+++ src/routes/Sells/index.ts
@@ -8,6 +8,7 @@ router.get('/get', isAuth as never, Sells.getSellList);
 router.get('/getSingle/:id', isAuth as never, Sells.getSell);
 router.post('/create', Sells.createSell);
 router.put('/edit/:id', isAuth as never, Sells.editSell);
+router.put('/toggleFinished/:id', isAuth, Sells.toggleFinished)
 router.delete('/delete/:id', isAuth as never, Sells.deleteSell);
 
 export = router;
